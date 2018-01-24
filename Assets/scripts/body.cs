a04e7c29bb506fa7d84774473669bfe41c9671b8
GERUBANA\User <iamzero666@gmail.com>
2018-01-25 01:58:38 +0800

    音效BUG調整完畢 加入特效(爆炸、煙霧、子彈音效) 待處理：2P問題

--- Assets/scripts/body.cs
+++ Assets/scripts/body.cs
@@ -9,23 +9,30 @@ public class body : MonoBehaviour {
 	//彈珠及發射器
 	public GameObject marble_ball;
 	public GameObject Fire;
+	public GameObject Fire_smoke;
 	//音效
 	public AudioClip[] this_audio_clip = new AudioClip[5];
 	public AudioSource this_audio;
+
 	//複製彈珠
 	private GameObject marble_ball_ins;
+	private GameObject Fire_smoke_ins;
+	//音效控制
+	private bool audioIsPlaying = false;
+	private bool audioIsChange = false;
 
 	// Use this for initialization
 	void Start () {
 		//Debug.Log (Goble_Player.playerName);
 		if(Goble_Player.playerName == null)
 			Goble_Player.playerName = this.name;
-		//Debug.Log (Goble_Player.playerName);
+		Debug.Log (Goble_Player.playerName);
 
 		this_audio = GetComponent<AudioSource> ();
 		this_audio.clip = this_audio_clip[0];
 		this_audio.Play ();
 		this_audio.loop = true;
+		audioIsPlaying = true;
 	}
 	
 	// Update is called once per frame
@@ -37,7 +44,7 @@ public class body : MonoBehaviour {
 	void OnTriggerEnter(Collider object_)
 	{
 		if (object_.tag == "ball") {
-
+			object_.GetComponent<marble_ball> ().explosion();
 			Destroy (object_.gameObject);
 		}
 	}
@@ -53,20 +60,45 @@ public class body : MonoBehaviour {
 	private void shoot()
 	{
 		marble_ball_ins = Instantiate(marble_ball, Fire.transform.position, transform.rotation)as GameObject;
+		Fire_smoke_ins = Instantiate(Fire_smoke, Fire.transform.position, transform.rotation)as GameObject;
 		marble_ball_ins.transform.Translate (0, 0, shoot_speed * Time.fixedDeltaTime);
 	}
 
 	public void moveAudio()
 	{
-		this_audio.clip = this_audio_clip[1];
-		this_audio.Play ();
-		this_audio.loop = true;
+		if(this_audio.clip != this_audio_clip[1])
+			audioIsChange = true;
+
+		audioPlay (this_audio_clip[1]);
 	}
 
 	public void moveAudio_stop()
 	{
-		this_audio.clip = this_audio_clip[0];
-		this_audio.Play ();
-		this_audio.loop = true;
+		if(this_audio.clip != this_audio_clip[0])
+			audioIsChange = true;
+		
+		audioPlay (this_audio_clip[0]);
 	}
+
+	private void audioPlay(AudioClip playclip)
+	{
+		this_audio.clip = playclip;
+
+		if (audioIsChange) {
+			this_audio.Play ();
+			this_audio.loop = true;
+			audioIsPlaying = true;
+			audioIsChange = false;
+		}
+	}
+
+	private void audioStop()
+	{
+		if (audioIsPlaying) {
+			this_audio.Stop ();
+			this_audio.loop = false;
+			audioIsPlaying = false;
+		}
+	}
+
 }
--- Assets/scripts/marble_ball.cs
+++ Assets/scripts/marble_ball.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class marble_ball : MonoBehaviour {
+	//不同爆炸模式
+	public GameObject hit_small;
+	public GameObject hit_big;
+	public GameObject hit_over;
+	//產生爆炸效果
+	private GameObject Fire_smoke_ins;
 
 	private float speed;
 	private GameObject player;
@@ -19,4 +25,9 @@ public class marble_ball : MonoBehaviour {
 		this.transform.Translate (0, 0, speed * Time.fixedDeltaTime);
 	}
 
+	public void explosion()
+	{
+		
+		Fire_smoke_ins = Instantiate(hit_small, transform.position, transform.rotation)as GameObject;
+	}
 }
